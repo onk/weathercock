644f43d0b7b3d8f711099c4037bc02da22210cf7
Takafumi ONAKA <takafumi.onaka@gmail.com>
2026-04-15 01:35:24 +0900

    Implement Weathercock.configure and Config class

--- lib/weathercock.rb
+++ lib/weathercock.rb
@@ -4,5 +4,22 @@ require_relative "weathercock/version"
 
 module Weathercock
   class Error < StandardError; end
-  # Your code goes here...
+
+  class Config
+    attr_accessor :namespace, :redis
+
+    def initialize
+      @namespace = "weathercock"
+    end
+  end
+
+  class << self
+    def configure
+      yield config
+    end
+
+    def config
+      @config ||= Config.new
+    end
+  end
 end
--- spec/weathercock_spec.rb
+++ spec/weathercock_spec.rb
@@ -5,7 +5,30 @@ RSpec.describe Weathercock do
     expect(Weathercock::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe ".configure" do
+    around do |example|
+      original = Weathercock.instance_variable_get(:@config)
+      example.run
+      Weathercock.instance_variable_set(:@config, original)
+    end
+
+    it "yields a config object" do
+      expect { |b| Weathercock.configure(&b) }.to yield_with_args(Weathercock::Config)
+    end
+
+    it "namespace defaults to 'weathercock'" do
+      expect(Weathercock.config.namespace).to eq("weathercock")
+    end
+
+    it "sets namespace" do
+      Weathercock.configure { |c| c.namespace = "myapp" }
+      expect(Weathercock.config.namespace).to eq("myapp")
+    end
+
+    it "sets redis" do
+      fake_redis = double("redis")
+      Weathercock.configure { |c| c.redis = fake_redis }
+      expect(Weathercock.config.redis).to eq(fake_redis)
+    end
   end
 end
